f1a3eba48e1bfefd68ea1e3307050190e624f47c
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2022-03-13 23:43:38 +0300

    while cycle with zero break

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-isNumber = False
-while not isNumber:
-    a = input ("age?")
-    try:
-        a = int(a)
-        isNumber = True
-    except:
-        print ("Введено не число")
-if 35 == a:
-    print ("Попал в диапазон")
-else:
-    print ("Не попал в диапазон")
+a = ""
+while 0 != a:
+    isNumber = False
+    while not isNumber:
+        a = input ("age?")
+        try:
+            a = int(a)
+            isNumber = True
+        except:
+            print ("Введено не число")
+    if 0 == a : break
+    if 35 == a:
+        print ("Попал в диапазон")
+    else:
+        print ("Не попал в диапазон")
