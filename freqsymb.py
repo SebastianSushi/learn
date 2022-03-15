08db3dcf3e448c5685fe53bec3ad1aa1413b85ef
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2022-03-15 18:01:12 +0300

    right

--- freqsymb.py
+++ freqsymb.py
@@ -1,9 +1,17 @@
+#input string from keyboard
 sInput = input ("Введите слово ")
+#empty dict for chars calculation
 dict = {}
+#calculate
 for a in sInput:
     if a in dict:
         dict [a] += 1
     else:
         dict [a] = 1
-#Sorted = sorted (dict.items())
-print (sorted(dict, key=dict.get, reverse=True))
+#sort keys by chars count
+sortedKeys = sorted(dict, key=dict.get, reverse=True)
+#fill resul dictionary with right sort
+rDict = {}
+for a in sortedKeys: rDict[a] = dict[a]
+#printing result
+print (rDict)
